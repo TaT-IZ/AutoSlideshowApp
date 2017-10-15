17f63a7b7f9bb6188db09b064f57e48a98546d3b
TaT-IZ <30386024+TaT-IZ@users.noreply.github.com>
2017-10-15 16:48:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tatsuhiro/autoslideshowapp/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.tatsuhiro.autoslideshowapp;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    Timer mTimer;
+    double mTimerSec = 0.0;
+
+    Handler mHandler = new Handler();
+
+    Button mStartButton;
+    Button mBackButton;
+    Button mStartButtonPause;
+    Cursor cursor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStartButton = (Button) findViewById(R.id.mStartButton);
+        mBackButton = (Button) findViewById(R.id.mBackButton);
+        mStartButtonPause = (Button) findViewById(R.id.mStartButtonPause);
+
+
+        // Android 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                getContentsInfo();
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+            }
+            // Android 5系以下の場合
+        } else {
+            getContentsInfo();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public void onClick(View v) {
+        // 画像の情報を取得する
+        ContentResolver resolver = getContentResolver();
+        Cursor cursor = resolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                null, // 項目(null = 全項目)
+                null, // フィルタ条件(null = フィルタなし)
+                null, // フィルタ用パラメータ
+                null // ソート (null ソートなし)
+        );
+        if (v.getId() == R.id.mStartButton) {
+            cursor.moveToNext() ;
+                do {
+                    // indexからIDを取得し、そのIDから画像のURIを取得する
+                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                    Long id = cursor.getLong(fieldIndex);
+                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                    Log.d("ANDROID", "URI : " + imageUri.toString());
+                } while (cursor.moveToNext());
+
+            } else if (v.getId() == R.id.mBackButton) {
+               cursor.moveToPrevious();
+
+                   do {
+                       // indexからIDを取得し、そのIDから画像のURIを取得する
+                       int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                       Long id = cursor.getLong(fieldIndex);
+                       Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                       Log.d("ANDROID", "URI : " + imageUri.toString());
+                   } while (cursor.moveToPrevious());
+               }
+            }
+
+
+        private void getContentsInfo () {
+
+            // 画像の情報を取得する
+            ContentResolver resolver = getContentResolver();
+            Cursor cursor = resolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                    null, // 項目(null = 全項目)
+                    null, // フィルタ条件(null = フィルタなし)
+                    null, // フィルタ用パラメータ
+                    null // ソート (null ソートなし)
+            );
+            if (cursor.moveToFirst()) {
+                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                Long id = cursor.getLong(fieldIndex);
+                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                imageVIew.setImageURI(imageUri);
+            }
+            cursor.close();
+        }
+    }
