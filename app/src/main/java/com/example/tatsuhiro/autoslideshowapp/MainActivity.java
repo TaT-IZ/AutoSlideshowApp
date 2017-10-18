012934dcce5f21e9a40790d81997842f0034e941
TaT-IZ <30386024+TaT-IZ@users.noreply.github.com>
2017-10-18 19:39:27 +0200

    Having to be modified how to set up for Re-Start

--- app/src/main/java/com/example/tatsuhiro/autoslideshowapp/MainActivity.java
+++ app/src/main/java/com/example/tatsuhiro/autoslideshowapp/MainActivity.java
@@ -17,19 +17,19 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     Timer mTimer;
-    double mTimerSec = 0.0;
-
     Handler mHandler = new Handler();
 
     Button mStartButton;
     Button mBackButton;
-    Button mStartButtonPause;
+    Button mStartButtonPause;//保留
     Cursor cursor;
+    private TimerTask mTimerTask;
 
 
     @Override
@@ -56,77 +56,99 @@ public class MainActivity extends AppCompatActivity {
         } else {
             getContentsInfo();
         }
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_CODE:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getContentsInfo();
+        // getContensinfoで呼び出されたら以下の処理を行う
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (cursor.moveToNext()) {
+                    // indexからIDを取得し、そのIDから画像のURIを取得する
+                    setImageView();
+
+                } else {//一番最初にもどる処理
+                    cursor.moveToFirst();
+                    setImageView();
+
                 }
-                break;
-            default:
-                break;
-        }
+            }
+        });
+
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (cursor.moveToPrevious()) {
+                    setImageView();
+
+                } else {//一番最初にもどる処理
+                    cursor.moveToLast();
+                    setImageView();
+                }
+            }
+        });
+
+
+        mStartButtonPause.setOnClickListener(new View.OnClickListener()
+
+        {
+
+            @Override
+            public void onClick(View v)  {
+
+                mTimer = new Timer();
+                mTimer.schedule(new TimerTask() {
+
+                    @Override
+                    public void run() {
+
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                if (cursor.moveToNext()) {
+                                    setImageView();
+                                }
+                            }
+                        });
+                    }
+                }, 100, 2000);    // 最初に始動させるまで 100ミリ秒、ループの間隔を 秒 に設定
+            }
+        });
     }
 
+    private void setImageView() {
+        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+        Long id = cursor.getLong(fieldIndex);
+        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-    public void onClick(View v) {
+        ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+        imageVIew.setImageURI(imageUri);
+    }
+
+    private void getContentsInfo() {
+
         // 画像の情報を取得する
         ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
+        cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                 null, // 項目(null = 全項目)
                 null, // フィルタ条件(null = フィルタなし)
                 null, // フィルタ用パラメータ
                 null // ソート (null ソートなし)
         );
-        if (v.getId() == R.id.mStartButton) {
-            cursor.moveToNext() ;
-                do {
-                    // indexからIDを取得し、そのIDから画像のURIを取得する
-                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                    Long id = cursor.getLong(fieldIndex);
-                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-                    Log.d("ANDROID", "URI : " + imageUri.toString());
-                } while (cursor.moveToNext());
-
-            } else if (v.getId() == R.id.mBackButton) {
-               cursor.moveToPrevious();
-
-                   do {
-                       // indexからIDを取得し、そのIDから画像のURIを取得する
-                       int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                       Long id = cursor.getLong(fieldIndex);
-                       Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-                       Log.d("ANDROID", "URI : " + imageUri.toString());
-                   } while (cursor.moveToPrevious());
-               }
-            }
-
-
-        private void getContentsInfo () {
-
-            // 画像の情報を取得する
-            ContentResolver resolver = getContentResolver();
-            Cursor cursor = resolver.query(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                    null, // 項目(null = 全項目)
-                    null, // フィルタ条件(null = フィルタなし)
-                    null, // フィルタ用パラメータ
-                    null // ソート (null ソートなし)
-            );
-            if (cursor.moveToFirst()) {
-                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                Long id = cursor.getLong(fieldIndex);
-                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-                ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-                imageVIew.setImageURI(imageUri);
-            }
-            cursor.close();
+        if (cursor.moveToFirst()) {
+            setImageView();//
         }
     }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        Log.d("Android", "onDestroy");
+    }
+}
+
